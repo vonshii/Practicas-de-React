6f4a027c8ce8a4b78ce6f7e1b47cd5e070dbdc97
vonshii <germanvscheidt@gmail.com>
2022-08-09 01:10:42 -0300

    se limpio la app y se agrego bootstrapp

---
+++ Practicas/listatareas/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Titulo from "./components/Titulo";
+ 
+const App = () => {
+  return (
+  <div>
+    <Titulo>  </Titulo>
+  </div>
+  );
+}
+
+export default App;
---
+++ Practicas/listatareas/src/components/Titulo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <div>
+        <h1>Lista Tareas</h1>
+        <hr />
+      </div>
+    );
+};
+
+export default Titulo;
